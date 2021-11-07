99b828060df448e34402155bc7b45378a389ecaf
OriolsonRodriguez <oriolson.rodriguez>
2021-11-07 10:58:59 -0500

    Flask app now running in port 4000

--- src/app.py
+++ src/app.py
@@ -2,10 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-def getTags():
-    '''
-    
-    '''
 
 @app.route("/")
 def indexPage():
@@ -25,7 +21,9 @@ def showTags():
 @app.route("/shutdown", endpoint="shutdown")
 def showTags():
     '''
-    
+    nothing here
     '''
     return "shuting down"
 
+if __name__=='__main__':
+    app.run(host="0.0.0.0", port=4000)
