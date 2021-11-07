ce0142e13856bfc79404c270f41fa6bccc357d91
OriolsonRodriguez <oriolson.rodriguez>
2021-11-07 15:03:43 -0500

    docker-compose file for container created

--- startApp.sh
+++ startApp.sh
@@ -1,6 +1,12 @@
 sudo apt install docker.io
 sudo systemctl enable --now docker
 
+apt update
+apt install git
+git clone https://github.com/OriolsonRodriguez/terraform_python_test
+cd terraform_python_test
+git checkout develop
+
 cd src/
 
 sudo docker build -t flugel .
