e5ad8f5d50b3f87885d4032f99492ab530b8e686
OriolsonRodriguez <oriolson.rodriguez>
2021-11-08 12:14:06 -0500

    showtags func modified to use instance instead of resource

--- src/app.py
+++ src/app.py
@@ -1,7 +1,6 @@
 import boto3
 import urllib.request
 from flask import Flask
-from collections import defaultdict
 
 
 app = Flask(__name__)
@@ -16,8 +15,7 @@ def getEC2Instance():
     '''
     get current instance pointer to ec2 instance. Return object
     '''
-    ec2 = boto3.resource('ec2', region_name='us-east-1')
-    return ec2.instances.filter(InstanceIds = [str(getInstance_ID())])
+    return boto3.resource('ec2', region_name='us-east-1')
 
 @app.route("/")
 def indexPage():
@@ -26,24 +24,30 @@ def indexPage():
     '''
     return "This is the starting page of the simple App for Flugel.it test, This is running in EC2 instance with ID: " + str(getInstance_ID())
 
+
 @app.route("/tags", endpoint="tags")
 def showTags():
     '''
-    It shows the Tags for EC2 instance and S3 Bucket
+    It shows the Tags for EC2 instance and S3 Bucket. Return dict() of tags
     '''
     # Connect to EC2
-    ec2 = boto3.resource('ec2', region_name='us-east-1')
+    ec2 = getEC2Instance()
     ec2instance = ec2.Instance(str(getInstance_ID()))
-    
+    instance_tags = {}
 
-    return ec2instance.tags
+    for tags in ec2instance.tags:
+        instance_tags[tags["Key"]] = tags["Value"]
+
+    return instance_tags
+
 
 @app.route("/shutdown", endpoint="shutdown")
 def shutDown():
     '''
-    Shutdown instance.  Return None
+    Shutdown instance.  Return String:  message confirmation
     '''
-    getEC2Instance().terminate() 
+    ec2=getEC2Instance()
+    ec2.instances.filter(InstanceIds = [str(getInstance_ID())]).terminate() 
     return "shuting down EC2 instance"
 
 if __name__=='__main__':
